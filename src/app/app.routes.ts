56934935137ca29c88fb693c7dbbe206f5d1ea0f
OmarKnows <omarknows98@gmail.com>
2024-08-07 11:06:33 +0300

    setup application routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { ROUTES } from './constants/routes';
+import { UserListComponent } from './modules/users/user-list/user-list.component';
+import { UserDetailsComponent } from './modules/users/user-details/user-details.component';
+import { NotFoundComponent } from './modules/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: ROUTES.USER,
+        component: UserListComponent,
+      },
+      {
+        path: `${ROUTES.USER}/:id`,
+        component: UserDetailsComponent,
+      },
+    ],
+  },
+  { path: '**', component: NotFoundComponent },
+];
--- src/app/constants/routes.ts
+++ src/app/constants/routes.ts
@@ -1,3 +1,3 @@
 export enum ROUTES {
-  USER = 'user',
+  USER = 'users',
 }
