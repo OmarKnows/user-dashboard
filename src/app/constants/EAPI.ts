98edff1c1ee31d534e7b6e1d49175e447ff3433c
OmarKnows <omarknows98@gmail.com>
2024-08-07 16:58:17 +0300

    created eapi and fakeEnv constants

---
+++ src/app/constants/EAPI.ts
@@ -0,0 +1,3 @@
+export enum EAPI {
+  USERS = 'users',
+}
---
+++ src/app/constants/fakeEnv.ts
@@ -0,0 +1,5 @@
+// these values should be provided in an env file but for the sake of the task I will place them here.
+
+export enum FAKE_ENV {
+  BASE_URL = 'https://reqres.in/api/',
+}
