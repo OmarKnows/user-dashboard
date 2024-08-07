1d135d040775066b315205ad8660671d7a409f71
omarknows <omarknows98@gmail.com>
2024-08-07 21:15:00 +0300

    added back button to user details page

--- src/app/modules/users/user-details/user-details.component.ts
+++ src/app/modules/users/user-details/user-details.component.ts
@@ -8,13 +8,18 @@ import {
 } from '@angular/core';
 import { IUserModel } from '../dummyData';
 import { UsersService } from '../../../services/users.service';
+import { MatButtonModule } from '@angular/material/button';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { heroArrowLeft } from '@ng-icons/heroicons/outline';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent, RouterLink, MatButtonModule],
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.scss',
+  viewProviders: [provideIcons({ heroArrowLeft })],
 })
 export class UserDetailsComponent implements OnInit {
   private usersService = inject(UsersService);
