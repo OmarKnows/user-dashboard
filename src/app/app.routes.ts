db977e77447f3f9c901bf4b10a6e50dfb6de33fb
OmarKnows <omarknows98@gmail.com>
2024-08-07 12:12:05 +0300

    added home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,17 @@ import { ROUTES } from './constants/routes';
 import { UserListComponent } from './modules/users/user-list/user-list.component';
 import { UserDetailsComponent } from './modules/users/user-details/user-details.component';
 import { NotFoundComponent } from './modules/not-found/not-found.component';
+import { HomeComponent } from './modules/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
     children: [
+      {
+        path: ROUTES.HOME,
+        component: HomeComponent,
+      },
       {
         path: ROUTES.USER,
         component: UserListComponent,
