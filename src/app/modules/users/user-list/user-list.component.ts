692680e90b32afa4fb9b29224dd903429f8a4265
OmarKnows <omarknows98@gmail.com>
2024-08-07 14:57:20 +0300

    initial user list UI

--- src/app/modules/users/user-list/user-list.component.ts
+++ src/app/modules/users/user-list/user-list.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { IUserModel, users } from '../dummyData';
+import { CardComponent } from '../../../common/components/card/card.component';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, CardComponent],
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.scss'
+  styleUrl: './user-list.component.scss',
 })
 export class UserListComponent {
-
+  usersList: IUserModel[] = users;
 }
