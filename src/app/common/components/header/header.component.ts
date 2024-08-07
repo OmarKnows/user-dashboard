8b6a1c5a1a2176fd53a41c4b3f06b7cc479bb506
OmarKnows <omarknows98@gmail.com>
2024-08-07 15:25:47 +0300

    changed header design

--- src/app/common/components/header/header.component.ts
+++ src/app/common/components/header/header.component.ts
@@ -4,11 +4,23 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ROUTES } from '../../../constants/routes';
 import { RouterLink } from '@angular/router';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import {
+  bootstrapGithub,
+  bootstrapGrid1x2Fill,
+} from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink,
+    NgIconComponent,
+  ],
+  viewProviders: [provideIcons({ bootstrapGithub, bootstrapGrid1x2Fill })],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
