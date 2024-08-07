bac0c5829eb0febf9ce33ff51cc528772f0957a9
OmarKnows <omarknows98@gmail.com>
2024-08-07 15:07:19 +0300

    initial user details ui

--- src/app/modules/users/user-details/user-details.component.ts
+++ src/app/modules/users/user-details/user-details.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { IUserModel, users } from '../dummyData';
 
 @Component({
   selector: 'app-user-details',
   standalone: true,
   imports: [],
   templateUrl: './user-details.component.html',
-  styleUrl: './user-details.component.scss'
+  styleUrl: './user-details.component.scss',
 })
 export class UserDetailsComponent {
-
+  user: IUserModel = users[0];
+  formattedName: string = this.user.first_name + ' ' + this.user.last_name;
 }
